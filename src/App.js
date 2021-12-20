4ae7db651dd48a33d47243b4bb85dc89c5bb5ef0
Elisa Mendoza <elisa.zamarripa89@gmail.com>
2021-12-20 17:01:27 +0900

    Added Search Engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from "./logo.png";
 import "./App.css";
 
+import Dictionary from "./Dictionary";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="text-center">Dictionary</div>
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <div className="text-center">Dictionary</div>
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <small>
+          <footer className="App-footer">
+            Coded by Elisa Mendoza Zamarripa
+          </footer>
+        </small>
+      </div>
     </div>
   );
 }
