5df72e7bce3b0e7f16ad674cab8a54d7ce098b3c
Elisa Mendoza <elisa.zamarripa89@gmail.com>
2021-12-21 20:49:14 +0900

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,21 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h2>{props.meaning.partOfSpeech}</h2>
+      <h2 className="text-capitalize">{props.meaning.partOfSpeech}</h2>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
-            <em>{definition.example}</em>
+            <p className="text-capitalize">
+              <strong>Definition: </strong>
+              {definition.definition}
+              <br />
+              <strong>Example: </strong>
+              <em>{definition.example}.</em>
+            </p>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
